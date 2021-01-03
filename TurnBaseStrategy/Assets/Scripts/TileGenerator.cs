0bcf4eee8029a65de8fac43df675e8513c4d701e
Gideon Francis <gideonnfrancis999@gmail.com>
2021-01-03 16:20:53 +0800

    Pushing base project

---
+++ TurnBaseStrategy/Assets/Scripts/TileGenerator.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileGenerator : MonoBehaviour
+{
+    [Tooltip("Starting Position of the map")]
+    public Transform startingPosition;
+    [Tooltip("Tile prefab to instantiate")]
+    public GameObject tilePrefab;
+    [Header("Settings for the map array")]
+    [Tooltip("Width of the map")]
+    public int mapWidth;
+    [Tooltip("Height of the map")]
+    public int mapHeight;
+    [Tooltip("Size of the tile")]
+    public int tileSize;
+
+    [System.NonSerialized]
+    public List<GameObject> ListOfTiles = new List<GameObject>();
+
+    public static TileGenerator instance = null;
+
+    private void Awake()
+    {
+        if (instance == null)
+            instance = this;
+        else if (instance != this)
+            Destroy(gameObject);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InitMap();
+    }
+
+    void InitMap()
+    {
+        // Loop through based on how big the map is set to be
+        for (int w = 0; w < mapWidth; ++w)
+        {
+            for (int h = 0; h < mapHeight; ++h)
+            {
+                // Instantiate a new map tile
+                //TODO: use object pooler for things like this
+                GameObject newTile = Instantiate(tilePrefab, this.transform);
+
+                // Set the position as it iterates
+                Vector3 newPosition = startingPosition.position;
+
+                newPosition.x += h * tileSize;
+                newPosition.z += w * tileSize;
+
+                newTile.transform.position = newPosition;
+
+                // Add it to the list
+                ListOfTiles.Add(newTile);
+            }
+        }
+    }
+}
---
+++ TurnBaseStrategy/Assets/Scripts/TilePicker.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TilePicker : MonoBehaviour
+{
+    public GameObject TestCharacter;
+
+    Vector3 TargetPosition;
+    public bool isMoving = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // on mouse click
+        if (Input.GetMouseButtonDown(0))
+        {
+            // Cast a ray from mouse cursor
+            RaycastHit[] hits;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            // Raycast to check for collided targets
+            hits = Physics.RaycastAll(ray, 100.0f);
+           
+            // Loop through the raycast targets
+            for (int i = 0; i < hits.Length; i++)
+            {
+                RaycastHit hit = hits[i];
+
+                // if it collides with a tile
+                if (hit.transform.tag == "Tile")
+                {
+                    Debug.Log("Hit a tile");
+                    Debug.Log("Tile Position " + hit.transform.position);
+
+                    if (isMoving == false)
+                    {
+                        int index = TileGenerator.instance.ListOfTiles.IndexOf(hit.transform.gameObject);
+                        if (index >= 0)
+                        {
+                            // Retrieve that tile
+                            GameObject targetTile = TileGenerator.instance.ListOfTiles[index];
+                            // Set the target position for the character
+                            TargetPosition = targetTile.transform.position;
+
+                            // Set isMoving
+                            isMoving = true;
+                        }
+                    }
+                    // Return after finishing what i needa do
+                    return;
+                }
+            }
+        }
+   
+        // if its moving
+        if (isMoving)
+        {
+            // Get the direction to move towards to
+            Vector3 dir = TargetPosition - TestCharacter.transform.position;
+            dir.y = 0;
+            // Normalize it
+            dir.Normalize();
+
+            TestCharacter.transform.position += dir * Time.deltaTime * 7.0f;
+
+            
+        }
+    }
+}
